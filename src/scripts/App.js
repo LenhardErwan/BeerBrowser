97633ad340b53588f26a7395500f1fb30cc5a5ae
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2020-03-03 15:22:58 +0100

    Starting Beer render

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,44 +1,97 @@
-import React from 'react'
+import React, { Component, Suspense } from 'react'
 import ReactDom from 'react-dom'
 import Model from './Model.js'
 
-class Beer extends React.Component {
+class Beer extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      name: props.name
+      name: props.name,
+      tagline: props.tagline,
+      description: props.description,
+      abv: props.abv,
+      ibu: props.ibu,
+      ebc: props.ebc,
+      srm: props.srm,
+      ph: props.ph,
+      attenuation_level: props.attenuation_level,
+      first_brewed: props.first_brewed,
+      food_pairing: props.food_pairing,
+      image_url: props.image_url,
+      volume: props.volume,
+      boil_volume: props.boil_volume,
+      ingredients: props.ingredients,
+      brewers_tips: props.brewers_tips
     }
+  }
 
-    console.log()
+  componentWillUpdate(nextProps, nextState) {
+    if(this.props !== nextProps) {
+      this.setState({
+        name: nextProps.name,
+        tagline: nextProps.tagline,
+        description: nextProps.description,
+        abv: nextProps.abv,
+        ibu: nextProps.ibu,
+        ebc: nextProps.ebc,
+        srm: nextProps.srm,
+        ph: nextProps.ph,
+        attenuation_level: nextProps.attenuation_level,
+        first_brewed: nextProps.first_brewed,
+        food_pairing: nextProps.food_pairing,
+        image_url: nextProps.image_url,
+        volume: nextProps.volume,
+        boil_volume: nextProps.boil_volume,
+        ingredients: nextProps.ingredients,
+        brewers_tips: nextProps.brewers_tips
+      })
+    }
   }
 
   render () {
     return (
-      <p>{this.state.name}</p>
+      <article>
+        <div>{/* Return Buttun */}</div>
+        <header>
+          <div className="img_container">
+            <img src={this.state.image_url ? `${this.state.image_url}` : `${this.state.no_img}` } height="200vh"/>
+          </div>
+          <div className="title_container">
+            <h1>{this.state.name}</h1>
+            <h2>"{this.state.tagline}"</h2>
+          </div>
+        </header>
+      </article>
     )
   }
 }
 
-class App extends React.Component {
+class App extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      beerName: 'trop bien'
+      beer: null
     }
+
   }
 
+  async componentDidMount() {
+    let beer = await Model.getRandomBeer();
+    this.setState({beer: beer});
+  }
+ 
   render () {
     return (
-      <Beer name={ this.state.beerName } />
+      <Beer { ...this.state.beer } />
     )
   }
 }
 
 ReactDom.render(
   <App />,
-  document.querySelector('#root')
+  document.querySelector('#app')
 )
 
 /*import Model from "./Model.js"
