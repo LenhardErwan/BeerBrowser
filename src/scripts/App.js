b83548a1fa0825f6c8c99555738d8c329f08b84a
Jeremy-JRM <39135270+Jeremy-JRM@users.noreply.github.com>
2020-03-10 10:58:50 +0100

    Adding nav input Adding refresh list with search

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -116,19 +116,82 @@ class Nav extends Component {
     super(props)
 
     this.state = {
-      name: '',
-      yeast: '',
-      malt: '',
-      food: '',
-      abv_lower: '',
-      abv_upper: '',
-      ibu_lower: '',
-      ibu_upper: '',
-      ebc_lower: '',
-      ebc_upper: '',
-      brewed_before: '',
-      brewed_after: ''
+      filtersTop: "-90px",
+      search: {
+        name: '',
+        yeast: '',
+        malt: '',
+        food: '',
+        abv_lower: '',
+        abv_upper: '',
+        ibu_lower: '',
+        ibu_upper: '',
+        ebc_lower: '',
+        ebc_upper: '',
+        brewed_before: '',
+        brewed_after: ''
+      },
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleFiltersClick () {
+    if (this.state.filtersTop !== "40px") {
+      this.setState({ filtersTop: "40px" })
+    } else {
+      this.setState({ filtersTop: "-90px" })
+    }
+  }
+
+  handleInputChange (event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    const search = this.state.search;
+    search[name] = value;
+
+    this.setState({
+      search: search
+    });
+  }
+
+  handleSubmit (event) {
+    this.props.onClick(this.state.search);
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <div>
+        <nav>
+          <a id="random" href="#">Get a Random beer!</a>
+          <form id="search" onSubmit={this.handleSubmit}>
+            <input type="text" name="name" onChange={this.handleInputChange} value={this.state.search.name} placeholder="beername"/>
+            <button>Search</button>
+          </form>
+          <a id="open_filter" onClick={() => this.handleFiltersClick()}>Filters</a>
+        </nav>
+        <form id="search_with_filter" style={{top: this.state.filtersTop}}>
+          <div>
+            <label>yeast </label><input type="text" name="yeast" onChange={this.handleInputChange} value={this.state.search.yeast} placeholder="yeast"/>
+            <label>malt </label><input type="text" name="malt" onChange={this.handleInputChange} value={this.state.search.malt} placeholder="malt"/>
+            <label>food </label><input type="text" name="food" onChange={this.handleInputChange} value={this.state.search.food} placeholder="food"/>
+          </div>
+          <div>
+            <label>ABV </label><input type="number" name="abv_lower" onChange={this.handleInputChange} value={this.state.search.abv_lower} placeholder="min" min="0" max="100"/><input type="number" name="abv_upper" onChange={this.handleInputChange} value={this.state.search.abv_upper} placeholder="max" min="0" max="100"/>
+            <label>IBU </label><input type="number" name="ibu_lower" onChange={this.handleInputChange} value={this.state.search.ibu_lower} placeholder="min" min="1" max="1200"/><input type="number" name="ibu_upper" onChange={this.handleInputChange} value={this.state.search.ibu_upper} placeholder="max" min="1" max="1200"/>
+            <label>EBC </label><input type="number" name="ebc_lower" onChange={this.handleInputChange} value={this.state.search.ebc_lower} placeholder="min" min="1" max="600"/><input type="number" name="ebc_upper" onChange={this.handleInputChange} value={this.state.search.ebc_upper} placeholder="max" min="1" max="600"/>
+          </div>
+          <div>
+            <label>Brewed before </label><input type="text" name="brewed_before" onChange={this.handleInputChange} value={this.state.search.brewed_before} placeholder="mm-yyyy"/>
+            <label>Brewed after </label><input type="text" name="brewed_after" onChange={this.handleInputChange} value={this.state.search.brewed_after} placeholder="mm-yyyy"/>
+          </div>
+          <input type="submit" id="submit" />
+        </form>
+      </div>
+    )
   }
 }
 
@@ -161,10 +224,14 @@ class App extends Component {
   }
 
   handleClick (beer) {
-    this.setState({beer: beer})
+    this.setState({beer: beer});
   }
 
-  async componentDidMount() {
+  handleSearch (search) {
+    this.setState({search: search});
+  }
+
+  async componentDidMount () {
     try {
       let beer = await Model.getRandomBeer();
       let beers = await Model.getBeersFilter(this.state.search, 10);
@@ -174,11 +241,26 @@ class App extends Component {
       Toastr.error(e, "Error:", this.state.toastr_error_options);
     }
   }
+
+  async componentWillUpdate (nextProps, nextState) {
+    if (this.state.search !== nextState.search) {
+      try {
+        let beers = await Model.getBeersFilter(nextState.search, 10);
+        this.setState({beers: beers});
+      }
+      catch (e) {
+        Toastr.error(e, "Error:", this.state.toastr_error_options);
+      }
+    }
+  }
  
   render () {
     return (
-      <List beers={ this.state.beers } onClick={(i) => this.handleClick(i)} />
-      //<Beer { ...this.state.beer } />
+      <div>
+        <Nav onClick={(i) => this.handleSearch(i)} />
+        <List beers={ this.state.beers } onClick={(i) => this.handleClick(i)} />
+        <Beer { ...this.state.beer } />
+      </div>
     )
   }
 }
